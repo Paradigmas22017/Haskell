c57cf44418105a88f3c14d730283430ac1eb9eae
Tulio Coelho <tulioopc@gmail.com>
2017-09-02 10:00:01 -0300

    Adding bomb x's and y's; player x's and y's; bomb and player values to gameloop; but gameloop in menuInitGame is reading only 7 arguments

--- Grid.hs
+++ Grid.hs
@@ -37,8 +37,9 @@ newArray i j arr = setValue (i, j) 5 arr
 
 --moveD arr posI posY = gameLoop ((setValue posI+1 posY arr) (posI+1) posY)
 
-printNewMatrix x y value arr = do
-	repeatNTimes (printArray (setValue (x, y) value arr)) (-1)
+printNewMatrix player_x player_y bomb_x bomb_y player_value bomb_value arr
+	| bomb_x /= -1 && bomb_y /= -1 = repeatNTimes (printArray (setValue (player_x, player_y) player_value (setValue ((bomb_x), bomb_y) bomb_value arr))) (-1)
+	| bomb_x == -1 && bomb_y == -1 = repeatNTimes (printArray (setValue (player_x, player_y) player_value arr)) (-1)
 
 --49 é obtido fazendo-se repeatNTimes length arrFinal no terminal
 --repeatNTimes :: Array (Int, Int) Int -> Int
--- Player.hs
+++ Player.hs
@@ -15,51 +15,57 @@ playerJ = 1
 
 -- Definindo constante para player e para bomba,
 --- serão passadas para o gameloop gerar uma nova matriz
+player :: Int
 player = 5
+
+bomb :: Int
 bomb = 7
+
 
 moveRight :: Int -> Int -> Array (Int, Int) Int -> IO()
 moveRight i j arr = case takeOneElement (i+1) j arr of
-	0 -> gameLoop (i+1) j (setValue ((i+1), j) 5 (setValue ((i), j) 0 arr)) player;
-	1 -> gameLoop i j arr player;
-	9 -> gameLoop i j arr player;
+	0 -> gameLoop (i+1) j (-1) (-1) player bomb (setValue ((i+1), j) 5 (setValue ((i), j) 0 arr));
+	1 -> gameLoop i j (-1) (-1) player bomb arr;
+	9 -> gameLoop i j (-1) (-1) player bomb arr;
 
 moveLeft :: Int -> Int -> Array (Int, Int) Int -> IO()
 moveLeft i j arr = case takeOneElement (i-1) j arr of
-	0 -> gameLoop (i-1) j (setValue ((i-1), j) 5 (setValue ((i), j) 0 arr)) player;
-	1 -> gameLoop i j arr player;
-	9 -> gameLoop i j arr player;
+	0 -> gameLoop (i-1) j (-1) (-1) player bomb (setValue ((i-1), j) 5 (setValue ((i), j) 0 arr));
+	1 -> gameLoop i j (-1) (-1) player bomb arr;
+	9 -> gameLoop i j (-1) (-1) player bomb arr;
 
 moveUp :: Int -> Int -> Array (Int, Int) Int -> IO()
 moveUp i j arr = case takeOneElement i (j-1) arr of
-	0 -> gameLoop i (j-1) (setValue (i, (j-1)) 5 (setValue ((i), j) 0 arr)) player;
-	1 -> gameLoop i j arr player;
-	9 -> gameLoop i j arr player;
+	0 -> gameLoop i (j-1) (-1) (-1) player bomb (setValue (i, (j-1)) 5 (setValue ((i), j) 0 arr));
+	1 -> gameLoop i j (-1) (-1) player bomb arr;
+	9 -> gameLoop i j (-1) (-1) player bomb arr;
 
 moveDown :: Int -> Int -> Array (Int, Int) Int -> IO()
 moveDown i j arr = case takeOneElement i (j+1) arr of
-	0 -> gameLoop i (j+1) (setValue (i, (j+1)) 5 (setValue ((i), j) 0 arr)) player;
-	1 -> gameLoop i j arr player;
-	9 -> gameLoop i j arr player;
+	0 -> gameLoop i (j+1) (-1) (-1) player bomb (setValue (i, (j+1)) 5 (setValue ((i), j) 0 arr));
+	1 -> gameLoop i j (-1) (-1) player bomb arr;
+	9 -> gameLoop i j (-1) (-1) player bomb arr;
 
 putBomb :: Int -> Int -> Array (Int, Int) Int -> IO()
 putBomb i j arr = case takeOneElement i j arr of
-	5 -> gameLoop i j arr bomb;
-	1 -> gameLoop i j arr player;
-	9 -> gameLoop i j arr player;
+	5 -> gameLoop i j i j player bomb arr;
+	1 -> gameLoop i j (-1) (-1) player bomb arr;
+	9 -> gameLoop i j (-1) (-1) player bomb arr;
 
-gameLoop :: Int -> Int -> Array (Int, Int) Int -> Int -> IO()
+-- player_x player_y bomb_x bomb_y player_value bomb_value arr
+gameLoop :: Int -> Int -> Int -> Int -> Int -> Int-> Array (Int, Int) Int -> IO()
 -- element deve ser player ou bomb
-gameLoop i j arr element = do { printNewMatrix i j element arr;
+gameLoop player_x player_y bomb_x bomb_y player_value bomb_value arr = do {
+				printNewMatrix player_x player_y bomb_x bomb_y  player_value bomb_value arr;
         opcao <- getChar;
-        actionConditions opcao i j arr element
+        actionConditions opcao player_x player_y bomb_x bomb_y player_value bomb_value arr
 	}
 
-actionConditions :: Char -> Int -> Int -> Array (Int, Int) Int -> Int -> IO()
-actionConditions opcao i j arr element
-	| opcao == 'd' = moveRight i j arr
-	| opcao == 'a' = moveLeft i j arr
-	| opcao == 'w' = moveUp i j arr
-	| opcao == 's' = moveDown i j arr
-	| opcao == 'c' = putBomb i j arr
-	| otherwise = gameLoop i j arr element
+actionConditions :: Char -> Int -> Int -> Int -> Int -> Int-> Int -> Array (Int, Int) Int -> IO()
+actionConditions opcao player_x player_y bomb_x bomb_y player bomb arr
+	| opcao == 'd' = moveRight player_x player_y arr
+	| opcao == 'a' = moveLeft player_x player_y arr
+	| opcao == 'w' = moveUp player_x player_y arr
+	| opcao == 's' = moveDown player_x player_y arr
+	| opcao == 'c' = putBomb player_x player_y arr
+	| otherwise = gameLoop player_x player_y bomb_x bomb_y player bomb arr
