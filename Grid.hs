4341c040b74a0256213dc7ca52919e76af78aa2e
Marcelo Augusto <mekmay@hotmail.com>
2017-09-04 00:59:49 -0300

    Refactoring print of matrix

--- Grid.hs
+++ Grid.hs
@@ -1,5 +1,5 @@
-module Grid(initialGrid, printArray, newArray, repeatNTimes,
-	repeatNTimesWithoutSpaces, arrFinal, takeOneElement, setValue, printNewMatrix, findElement, makeExplosion, finishGame) where
+module Grid(initialGrid, printArray, newArray, arrFinal, takeOneElement, setValue, printNewMatrix,
+	findElement, makeExplosion, finishGame) where
 
 import Data.Array
 import Data.List.Split
@@ -13,7 +13,6 @@ gridWidth = 10
 
 gridHeight :: Int
 gridHeight = 10
-
 
 initialGrid :: Array (Int, Int) Int
 initialGrid =
@@ -31,43 +30,21 @@ generator x y
 	| otherwise = 0
 
 printArray arr =
-  unlines [unwords [show (arr ! (x, y)) | x <- [0..gridWidth]] | y <- [0..gridHeight]]
+	unlines [unwords [show (arr ! (x, y)) | x <- [0..gridWidth]] | y <- [0..gridHeight]]
 
---newArray :: Int -> Int -> Array -> Array
+newArray :: Int -> Int -> Array(Int, Int) Int -> Array(Int, Int) Int
 newArray i j arr = setValue (i, j) 5 arr
 
---moveD arr posI posY = gameLoop ((setValue posI+1 posY arr) (posI+1) posY)
 printNewMatrix :: Int -> Int -> Int -> Int -> Int -> Int -> Array(Int, Int) Int -> IO()
 printNewMatrix player_x player_y bomb_x bomb_y player_value bomb_value arr =
 	if bomb_x /= (-1) && bomb_y /= (-1)
-		then repeatNTimes (printArray (setValue (player_x, player_y) bomb_value arr)) (-1)
+		then putStr ("\ESC[2J\n" ++ (printArray (setValue (player_x, player_y) bomb_value arr)))
 		else if bomb_x == (-1) && bomb_y == (-1)
-			then repeatNTimes (printArray (setValue (player_x, player_y) player_value arr)) (-1)
+			then putStr ("\ESC[2J\n" ++ (printArray (setValue (player_x, player_y) player_value arr)))
 			else print "Caso Errado"
-
---49 é obtido fazendo-se repeatNTimes length arrFinal no terminal
---repeatNTimes :: Array (Int, Int) Int -> Int
-repeatNTimes arr (-1) = do
-	putChar '\n'
-	repeatNTimes arr (0)
-repeatNTimes arr 242 = putChar '\n'
-repeatNTimes arr 241 = do
-	putChar((arr)!!241)
-	repeatNTimes arr (242)
-repeatNTimes arr n = do
-  	putChar((arr)!!n)
-  	repeatNTimes arr (n+1)
 
 arrFinal :: Array (Int, Int) Int -> String
 arrFinal arr = printArray arr
-
-repeatNTimesWithoutSpaces :: Array (Int, Int) Int -> Int -> IO()
-repeatNTimesWithoutSpaces arr 49 = putChar((arrFinal arr)!!49)
-repeatNTimesWithoutSpaces arr n = do
-  if ((arrFinal arr)!!n) /= ' '
-    then do putChar((arrFinal arr)!!n)
-            repeatNTimesWithoutSpaces arr (n+1)
-    else do repeatNTimesWithoutSpaces arr (n+1)
 
 takeOneElement :: Int -> Int -> Array (Int, Int) Int -> Int
 takeOneElement i j arr =  arr!(i,j)
@@ -86,7 +63,6 @@ findElement i j element arr
 	| otherwise = [-10, -10] -- Caso de erro
 
 
-
 makeExplosion :: Int -> Int -> [Int] -> Int -> Int -> Array (Int, Int) Int -> Array (Int, Int) Int
 makeExplosion player_x player_y bomb_position player bomb arr =
 	setValue (bomb_position!!0, bomb_position!!1) 0
@@ -95,17 +71,16 @@ makeExplosion player_x player_y bomb_position player bomb arr =
 				(isExplosible [bomb_position!!0, bomb_position!!1+1]
 					(isExplosible [bomb_position!!0, bomb_position!!1-1] arr))))
 
-
 isExplosible :: [Int] -> Array (Int, Int) Int-> Array (Int, Int) Int
 isExplosible bomb_position arr
 	| (arr ! (bomb_position!!0, bomb_position!!1)) /= 9 = setValue (bomb_position!!0, bomb_position!!1) 0 arr
 	| otherwise = arr
 
-
 finishGame :: Array (Int, Int) Int -> IO()
 finishGame arr = do {
-	putStr (printArray arr);
-	threadDelay 2500000;
-	putStr "\ESC[2J";
+	clear;
 	putStrLn "\t\t\t\tThe end!\n\n\n\n\n\n\n\n\n\n\n\n\n";
 }
+
+clear :: IO()
+clear = putStr "\ESC[2J\n"
